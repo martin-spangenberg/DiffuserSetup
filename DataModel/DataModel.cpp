6d2e013715319066b30614c2d986582de0a95212
Martin Spangenberg <m.spangenberg.1@warwick.ac.uk>
2021-03-30 13:33:49 +0100

    Comment out functions referring to DfTree in DataModel.cpp

--- DataModel/DataModel.cpp
+++ DataModel/DataModel.cpp
@@ -2,34 +2,34 @@
 
 DataModel::DataModel(){}
 
-DfTree* DataModel::GetDfTree(std::string name)
-{
-//  std::cout << "map size " << m_DfTrees.size() << std::endl;
-//  std::cout << "GETTING from map " << name << "." << std::endl;
-  if (m_dftrees.count(name))
-    return m_dftrees[name];
-  else
-    return 0;
-}
+// DfTree* DataModel::GetDfTree(std::string name)
+// {
+// //  std::cout << "map size " << m_DfTrees.size() << std::endl;
+// //  std::cout << "GETTING from map " << name << "." << std::endl;
+//   if (m_dftrees.count(name))
+//     return m_dftrees[name];
+//   else
+//     return 0;
+// }
 
-void DataModel::AddDfTree(std::string name, DfTree *tree)
-{
-  std::cout << "map size " << m_dftrees.size() << std::endl;
-  std::cout << "ADDING to map " << name << ",\t" << tree << "." << std::endl;
-  m_dftrees[name]=tree;
-  std::cout << m_dftrees[name] << std::endl;
-}
+// void DataModel::AddDfTree(std::string name, DfTree *tree)
+// {
+//   std::cout << "map size " << m_dftrees.size() << std::endl;
+//   std::cout << "ADDING to map " << name << ",\t" << tree << "." << std::endl;
+//   m_dftrees[name]=tree;
+//   std::cout << m_dftrees[name] << std::endl;
+// }
 
 
-void DataModel::DeleteDfTree(std::string name)
-{
-  std::cout << "map size " << m_dftrees.size() << std::endl;
-  std::cout << "REMOVING from map " << name << "." << std::endl;
-  delete m_dftrees[name];
-  m_dftrees.erase(name);
-}
+// void DataModel::DeleteDfTree(std::string name)
+// {
+//   std::cout << "map size " << m_dftrees.size() << std::endl;
+//   std::cout << "REMOVING from map " << name << "." << std::endl;
+//   delete m_dftrees[name];
+//   m_dftrees.erase(name);
+// }
 
-int DataModel::SizeDfTree()
-{
-  return m_dftrees.size();
-}
+// int DataModel::SizeDfTree()
+// {
+//   return m_dftrees.size();
+// }
